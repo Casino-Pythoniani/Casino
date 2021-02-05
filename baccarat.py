d342539d570b444087cb0b2625f0d712e542c080
ruben <virduzzoruben@gmail.com>
2021-02-05 17:03:01 +0100

    Modified bacarat file: added time module.

--- baccarat.py
+++ baccarat.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 dinero = 100
 final = True
 pos = [0,1]
@@ -12,15 +12,20 @@ while final:
             print("\nHas perdido.")
         final = False
     else:
+        time.sleep (0.5)
         elecc = int(input("\nElige tu posicion (1 por Jugador, 0 por Banco): "))
         if elecc in pos:
             print("\nDinero total: ",dinero)
+            time.sleep (0.5)
             apu = int(input("\nDinero a apostar: "))
+            time.sleep (0.5)
             if apu <= dinero:
                 PP = 0
+                time.sleep (2)
                 print("\nMano Jugador: ")              
                 n1 = random.randrange(2,13)
                 n2 = random.randrange(2,13)
+                time.sleep (1)
                 if n1 > 9:
                     n1 = 0
                 elif n2 > 9:
@@ -29,11 +34,14 @@ while final:
                 PP = n1 + n2
                 if PP > 9:
                     PP = PP - 10
+                    time.sleep (2)
                 print("\nPuntaje: ",PP)
                 PB = 0
+                time.sleep (2)
                 print("\nMano Banco: ")
                 N1 = random.randint(2,13)
                 N2 = random.randint(2,13)
+                time.sleep (1)
                 if N1 > 9:
                     N1 = 0
                 elif N2 > 9:
@@ -42,14 +50,17 @@ while final:
                 PB = N1 + N2
                 if PB > 9:
                     PB = PB -10
+                time.sleep (2)
                 print("\nPuntaje: ",PB)
                 if PP > PB:
+                    time.sleep (2)
                     print("\nGanador: Jugador")
                     if elecc == 1:
                         dinero = dinero + apu
                     else:
                         dinero = dinero - apu
                 elif PB > PP:
+                    time.sleep (2)
                     print("\nGanador: Banco")
                     if elecc == 0:
                         dinero = dinero + apu
